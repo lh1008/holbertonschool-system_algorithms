556f22af42cab0e0d511806d3af362ce6eb54249
lh1008 <paintluis@protonmail.com>
2021-05-21 18:04:23 -0500

    Adding first files for project 0x01 C-Graphs

---
+++ 0x01-graphs/0-graph_create.c
@@ -0,0 +1,11 @@
+#include "graphs.h"
+
+/**
+ * graph_create - entry to graph create
+ * Desc: graph_create function that allocates memory
+ * to store a graph_t structure
+ * Return: pointer to the allocated structure, or NULL on failure
+ */
+graph_t *graph_create(void)
+{
+}
