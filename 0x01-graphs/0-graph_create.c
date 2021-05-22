0811b85d5ba4431c356ed53084202169166f765e
lh1008 <paintluis@protonmail.com>
2021-05-22 18:14:45 -0500

    Updated with nnb_vertices and pointer to next NULL node

--- 0x01-graphs/0-graph_create.c
+++ 0x01-graphs/0-graph_create.c
@@ -9,13 +9,19 @@
 graph_t *graph_create(void)
 {
 	struct graph_s *graph_t;
+	size_t nb_vertices = 0, i;
 
 	graph_t = malloc(sizeof(graph_t));
 
 	if (graph_t == NULL)
 		return (NULL);
 
-	graph_t->vertices = (struct vertex_s*)malloc(sizeof(struct vertex_s));
+	graph_t->nb_vertices = nb_vertices;
+
+	graph_t->vertices = (struct vertex_s*)malloc(nb_vertices * sizeof(struct vertex_s));
+
+	for (i = 0; i < nb_vertices; ++i)
+		graph_t->vertices[i].next = NULL;
 
 	return (graph_t);
 
