48671fbfd0406a1a94629f2492460b9011b2e321
lh1008 <paintluis@protonmail.com>
2021-05-26 15:32:19 -0500

    Added depth-first traversal prototype to graphs header

--- 0x01-graphs/graphs.h
+++ 0x01-graphs/graphs.h
@@ -74,5 +74,7 @@ vertex_t *graph_add_vertex(graph_t *graph, const char *str);
 int graph_add_edge(graph_t *graph, const char *src,
 		   const char *dest, edge_type_t type);
 void graph_delete(graph_t *graph);
+size_t depth_first_traverse(const graph_t *graph,
+			    void (*action)(const vertex_t *v, size_t depth));
 
 #endif /* GRAPHS_H */
