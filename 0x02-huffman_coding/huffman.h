6b24384584ed122ce9b16a75aa74658e3a056244
lh1008 <paintluis@protonmail.com>
2021-06-15 16:47:15 -0500

    Added prototype header for task 6, main file, and file to add code

--- 0x02-huffman_coding/huffman.h
+++ 0x02-huffman_coding/huffman.h
@@ -18,5 +18,6 @@ typedef struct symbol_s
 } symbol_t;
 
 symbol_t *symbol_create(char data, size_t freq);
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size);
 
 #endif /* HUFFMAN_H */
---
+++ 0x02-huffman_coding/huffman_priority_queue.c
@@ -0,0 +1,14 @@
+#include "huffman.h"
+
+/**
+ * huffman_priority_queue - entry to huffman priority queue
+ * Desc: huffman_priority_queue function that creates a priority
+ * queue for the Huffman coding algorithm
+ * @data: char pointer array of characters of size size
+ * @freq: size_t pointer array containing associated frequencies
+ * @size: size_t size
+ * Return: pointer to the created min heap
+ */
+heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
+{
+}
