367e825e85f9abc7ffa375840d94dc1475d7eb59
lh1008 <paintluis@protonmail.com>
2021-06-15 17:42:50 -0500

    Finished code entry, compiled and working properly

--- 0x02-huffman_coding/huffman_priority_queue.c
+++ 0x02-huffman_coding/huffman_priority_queue.c
@@ -1,6 +1,23 @@
 #include "huffman.h"
 
 /**
+ * data_cmp - entry to data cmp
+ * Desc: data_cmp function that compares freq data in symbol struct
+ * @node_a: first node containing a symbol struct
+ * @node_b: second node containing a symbol struct
+ * Return: difference of the frequencies
+ */
+int data_cmp(void *node_a, void *node_b)
+{
+	symbol_t *symbol_a, *symbol_b;
+
+	symbol_a = ((binary_tree_node_t *)node_a)->data;
+	symbol_b = ((binary_tree_node_t *)node_b)->data;
+
+	return (symbol_a->freq - symbol_b->freq);
+}
+
+/**
  * huffman_priority_queue - entry to huffman priority queue
  * Desc: huffman_priority_queue function that creates a priority
  * queue for the Huffman coding algorithm
@@ -11,4 +28,18 @@
  */
 heap_t *huffman_priority_queue(char *data, size_t *freq, size_t size)
 {
+	heap_t *heap;
+	symbol_t *symbol;
+	binary_tree_node_t *node;
+	size_t i;
+
+	heap = heap_create(data_cmp);
+	for (i = 0; i < size; i++)
+	{
+		symbol = symbol_create(data[i], freq[i]);
+		node = binary_tree_node(NULL, symbol);
+		node = heap_insert(heap, node);
+	}
+
+	return (heap);
 }
