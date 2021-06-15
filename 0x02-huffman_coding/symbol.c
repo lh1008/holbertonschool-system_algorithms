db8dbea5f7779185ed630205640d1f4d5282ffee
lh1008 <paintluis@protonmail.com>
2021-06-14 20:13:03 -0500

    Added symbol.c file that creates a symbol_t data structure

---
+++ 0x02-huffman_coding/symbol.c
@@ -0,0 +1,23 @@
+#include "huffman.h"
+
+/**
+ * symbol_create - entry to symbol create
+ * Desc: symbol_create function that creates a symbol_t data structure
+ * @data: char type data to be storedin the structure
+ * @freq: size_t type associated frequency
+ * Return: pointer to the created structure, or NULL if it fails
+ */
+symbol_t *symbol_create(char data, size_t freq)
+{
+	symbot_t *node;
+
+	node = malloc(sizeof(symbol_t));
+
+	if (node == NULL)
+		return (NULL);
+
+	node->data = data;
+	node->freq = freq;
+
+	return (NULL);
+}
