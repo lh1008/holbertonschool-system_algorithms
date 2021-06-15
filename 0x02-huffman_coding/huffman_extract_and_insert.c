070b5f6f4d8bf3fccdfb22283eb6f61b9a0ae579
lh1008 <paintluis@protonmail.com>
2021-06-15 17:59:28 -0500

    Added extract huffman generic file

---
+++ 0x02-huffman_coding/huffman_extract_and_insert.c
@@ -0,0 +1,12 @@
+#include "huffman.h"
+
+/**
+ * huffman_extract_and_insert - entry to extract huffman function
+ * Desc: huffman_extract_and_insert function that extracts the two nodes
+ * of the priority queue and insert a new node
+ * @priority_queue: pointer to the priority queue node to extract
+ * Return: 1 on success or 0 on failure
+ */
+int huffman_extract_and_insert(heap_t *priority_queue)
+{
+}
