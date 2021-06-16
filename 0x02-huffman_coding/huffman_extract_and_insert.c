27ad81e9f616065ebb9eb640fe90ed800a06ecaa
lh1008 <paintluis@protonmail.com>
2021-06-16 17:35:57 -0500

    Added task 7 huffman extract code with compiled file

--- 0x02-huffman_coding/huffman_extract_and_insert.c
+++ 0x02-huffman_coding/huffman_extract_and_insert.c
@@ -9,4 +9,28 @@
  */
 int huffman_extract_and_insert(heap_t *priority_queue)
 {
+	binary_tree_node_t *node, *left, *right;
+	size_t sum;
+	symbol_t *symbol;
+
+	if (!priority_queue || priority_queue->size < 2)
+		return (0);
+
+	left = heap_extract(priority_queue);
+	right = heap_extract(priority_queue);
+	sum = ((symbol_t *)left->data)->freq;
+	sum += ((symbol_t *)right->data)->freq;
+	symbol = symbol_create(-1, sum);
+
+	if (!symbol)
+		return (0);
+
+	left->parent = right->parent = node = binary_tree_node(NULL, symbol);
+	node->left = left;
+	node->right = right;
+
+	if (heap_insert(priority_queue, node) != NULL)
+		return (0);
+
+	return (1);
 }
