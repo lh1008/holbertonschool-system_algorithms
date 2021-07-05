63b5fae8e5a4517f4bd990b644bb1bcbb6c99a80
lh1008 <paintluis@protonmail.com>
2021-07-05 18:21:20 -0500

    Added task 0 function clean file

---
+++ 0x03-pathfinding/0-backtracking_array.c
@@ -0,0 +1,17 @@
+#include "pathfinding.h"
+
+/**
+ * backtracking_array - entry to backtracking array
+ * Desc: backtracking_array function that searches for the path from a
+ * starting point to a target point within a two-dimensional array
+ * @map: pointer to a read-only two-dimensional array
+ * @rows: rows of map
+ * @col: columns of map
+ * @start: data structure to store the cordinates of the starting point
+ * @target: data structure to store the cordinates of the target point
+ * Return: return a queue, in which each node is a point in path
+ * from start to target
+ */
+queue_t *backtracking_array(char **map, int rows, int cols, point_t const *start, point_t const *target)
+{
+}
