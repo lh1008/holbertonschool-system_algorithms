f05018d3460ae8b999cbc01ead0fdd15b619d4d1
lh1008 <paintluis@protonmail.com>
2021-07-05 18:00:48 -0500

    Added header file, 'pathfinding.h'

---
+++ 0x03-pathfinding/pathfinding.h
@@ -0,0 +1,16 @@
+#ifndef PATHFINDING_H
+#define PATHFINDING_H
+
+/**
+ * struct point_s - Structure storing coordinates
+ *
+ * @x: X coordinate
+ * @y: Y coordinate
+ */
+typedef struct point_s
+{
+    int x;
+    int y;
+} point_t;
+
+#endif /* PATHFINDING_H */
