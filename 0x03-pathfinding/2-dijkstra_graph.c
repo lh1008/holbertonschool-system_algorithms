97d2537de4bac18ad3bfd9acda517a791da3415a
lh1008 <paintluis@protonmail.com>
2021-07-08 14:39:57 -0500

    Added task 2 file function

---
+++ 0x03-pathfinding/2-dijkstra_graph.c
@@ -0,0 +1,15 @@
+#include "pathfinding.h"
+
+/**
+ * dijkstra_graph - entry to dijkstra graph
+ * Desc: dijkstra_graph function that searches for the shortest
+ * path from a starting point to a target point in a graph
+ * @graph: pointer to the graph to go through
+ * @start: pointer to the starting vertex
+ * @target: pointer to the target vertex
+ * Return: queue from graph
+ */
+queue_t *dijkstra_graph(graph_t *graph, vertex_t const *start,
+			vertex_t const *target)
+{
+}
