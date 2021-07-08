4a7a9e06d0ab3fd317508d75392584646a1b1b28
lh1008 <paintluis@protonmail.com>
2021-07-08 11:20:08 -0500

    Betty requirement failed in header

--- 0x03-pathfinding/pathfinding.h
+++ 0x03-pathfinding/pathfinding.h
@@ -16,10 +16,11 @@
  */
 typedef struct point_s
 {
-    int x;
-    int y;
+	int x;
+	int y;
 } point_t;
 
-queue_t *backtracking_array(char **map, int rows, int cols, point_t const *start, point_t const *target);
+queue_t *backtracking_array(char **map, int rows, int cols,
+			    point_t const *start, point_t const *target);
 
 #endif /* PATHFINDING_H */
