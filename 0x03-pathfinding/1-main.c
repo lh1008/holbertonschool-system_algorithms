880f1723d5a2542d540163842c161610990bbc89
lh1008 <paintluis@protonmail.com>
2021-07-08 13:47:28 -0500

    Removed 2-main function, added task 1 main function

--- 0x03-pathfinding/1-main.c
+++ 0x03-pathfinding/1-main.c
@@ -22,7 +22,7 @@ static void print_free_path(queue_t *path)
 }
 
 /**
- * main - A simple graph to test Dijkstra's algorithm
+ * main - A simple graph to test Backtracking algorithm
  *
  * Return: Either EXIT_SUCCESS or EXIT_FAILURE
  */
@@ -43,19 +43,19 @@ int main(void)
 	graph_add_vertex(graph, "Houston", 0, 0);
 	graph_add_vertex(graph, "Nashville", 0, 0);
 	target = graph_add_vertex(graph, "Miami", 0, 0);
-	graph_add_edge(graph, "San Francisco", "Los Angeles", 347, BIDIRECTIONAL);
-	graph_add_edge(graph, "San Francisco", "Las Vegas", 417, BIDIRECTIONAL);
-	graph_add_edge(graph, "Los Angeles", "Las Vegas", 228, BIDIRECTIONAL);
-	graph_add_edge(graph, "San Francisco", "Seattle", 680, BIDIRECTIONAL);
-	graph_add_edge(graph, "Seattle", "Chicago", 1734, BIDIRECTIONAL);
-	graph_add_edge(graph, "Chicago", "Washington", 594, BIDIRECTIONAL);
-	graph_add_edge(graph, "Washington", "New York", 203, BIDIRECTIONAL);
-	graph_add_edge(graph, "Las Vegas", "Houston", 1227, BIDIRECTIONAL);
-	graph_add_edge(graph, "Houston", "Nashville", 666, BIDIRECTIONAL);
-	graph_add_edge(graph, "Nashville", "Washington", 566, BIDIRECTIONAL);
-	graph_add_edge(graph, "Nashville", "Miami", 818, BIDIRECTIONAL);
+	graph_add_edge(graph, "San Francisco", "Los Angeles", 0, BIDIRECTIONAL);
+	graph_add_edge(graph, "San Francisco", "Las Vegas", 0, BIDIRECTIONAL);
+	graph_add_edge(graph, "Los Angeles", "Las Vegas", 0, BIDIRECTIONAL);
+	graph_add_edge(graph, "San Francisco", "Seattle", 0, BIDIRECTIONAL);
+	graph_add_edge(graph, "Seattle", "Chicago", 0, BIDIRECTIONAL);
+	graph_add_edge(graph, "Chicago", "Washington", 0, BIDIRECTIONAL);
+	graph_add_edge(graph, "Washington", "New York", 0, BIDIRECTIONAL);
+	graph_add_edge(graph, "Las Vegas", "Houston", 0, BIDIRECTIONAL);
+	graph_add_edge(graph, "Houston", "Nashville", 0, BIDIRECTIONAL);
+	graph_add_edge(graph, "Nashville", "Washington", 0, BIDIRECTIONAL);
+	graph_add_edge(graph, "Nashville", "Miami", 0, BIDIRECTIONAL);
 
-	path = dijkstra_graph(graph, start, target);
+	path = backtracking_graph(graph, start, target);
 	if (!path)
 	{
 		fprintf(stderr, "Failed to retrieve path\n");
